40c229bcccbf35fda54cf9b50b61de47a6ff3bb2
yodaluc <luc.audabram1@gmail.com>
2019-09-23 16:01:03 +0200

    Ajout de la gestion de la base de données

---
+++ src/BDD/DbConnector.php
@@ -0,0 +1,37 @@
+<?php
+
+$GLOBALS["db_logpath"] = __DIR__ . 'php.log';
+$GLOBALS["bEchoLogs"] = false;
+$GLOBALS["bEnableLogging"] = true;
+$GLOBALS["bWriteLogs"] = true;
+
+class DbConnector
+{
+    /**
+     * @throws \Exception
+     */
+    public function getConnection()
+    {
+        $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ';charset=utf8';
+        try {
+            $pdo = new \PDO($dsn, DB_USER, DB_PASSWORD);
+        } catch (\PDOException $exception) {
+            throw new \Exception("No connection to database");
+        }
+        return $pdo;
+    }
+
+    public function prepStatement($pdo,$sql)
+    {
+        $req_prep = $pdo->prepare($sql);
+        return $req_prep;
+    }
+
+    public function execStatement($statement)
+    {
+        $statement->execute();
+        preg_replace('/\s+/S', " ", $statement->queryString);
+        $result = $statement->fetchAll(2);
+        return $result; // FETCH_ASSOC
+    }
+}
