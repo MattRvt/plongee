310b56aa147802eb8a82d59c326e90b4711f836c
Melissande <smelissande@gmail.com>
2019-09-25 16:31:21 +0200

    Formulaire qui marche mais il manque un truc

--- src/view/palanquee.php
+++ src/view/palanquee.php
@@ -1,25 +1,28 @@
-<?php require_once ("Header.php");?>
-<form name="palanquee" action= "" method="post" enctype="multipart/form-data">
+<?php 
+	require_once ("Header.php");
+	include ("util.php");
+?>
+<form name="palanquee" action= "traitementPalanquee.php" method="post" enctype="multipart/form-data">
 		<fieldset>
 		<legend><b>Créer une palanquée</b></legend>
 		<br />
-		<label for="date">Date : </label><input type="date" name="date" size="30" maxlength="50" <?php verifierRempli("date");?>><br />
+		<label for="date">Date : </label><input type="date" name="datePalanquee" size="30" maxlength="50" <?php verifierRempli("date");?>><br />
         <select name="seance" size="1">
-		    <option value="" <?php VerifSelect("seance","rien"); ?>>------</option>
-		    <option value="matin" <?php VerifSelect("seance","matin"); ?>>Matin</option>
-		    <option value="apres-Midi" <?php VerifSelect("seance","apres-midi")?>>Après-midi</option>
-		    <option value="soir" <?php VerifSelect("seance","apres-misoirdi")?>>Soir</option>
+		    <option value="" <?php VerifSelectSeance("seance","rien"); ?>>------</option>
+		    <option value="matin" <?php VerifSelectSeance("seance","matin"); ?>>Matin</option>
+		    <option value="apres-Midi" <?php VerifSelectSeance("seance","apres-midi")?>>Après-midi</option>
+		    <option value="soir" <?php VerifSelectSeance("seance","apres-misoirdi")?>>Soir</option>
         </select>
-        <label for="lieu">Lieu de la plongée : </label><input type="text" name="lieu" <?php verifierRempli("lieu");?>><br />
+        <label for="lieu">Lieu de la plongée : </label><input type="text" name="lieu" placeholder="Lieu?" <?php verifierRempli("lieu");?>><br />
         <select name="embarcation" size="1">
-            <option value="" <?php VerifSelect("embarcation","rien"); ?>>------</option>
-		    <option value="beclem" <?php VerifSelect("embarcation","beclem"); ?>>Beclem</option>
-		    <option value="estelenn" <?php VerifSelect("embarcation","estelem"); ?>>Estelenn</option>
+            <option value="" <?php VerifSelectEmbarcation("embarcation","rien"); ?>>------</option>
+		    <option value="beclem" <?php VerifSelectEmbarcation("embarcation","beclem"); ?>>Beclem</option>
+		    <option value="estelenn" <?php VerifSelectEmbarcation("embarcation","estelem"); ?>>Estelenn</option>
 		</select>
         <label for="effectifPlongeurs">Effectif de plongeurs : </label><input type="number" name="effectifPlongeurs" <?php verifierRempli("effectifPlongeurs");?>><br />
         <label for="effectifBateau">Effectif du bateau : </label><input type="number" name="effectifBateau" <?php verifierRempli("effectifBateau");?>><br />
-		<label for="directeurPlongee">Directeur de plongée : </label><input type="text" name="directeurPlongee" <?php verifierRempli("directeurPlongee");?>> <br />
-        <label for="securiteSurface">Sécurité de surface : </label><input type="text" name="securiteSurface" <?php verifierRempli("securiteSurface");?>> <br />
+		<label for="directeurPlongee">Directeur de plongée : </label><input type="text" name="directeurPlongee" placeholder="directeur de plongée?" <?php verifierRempli("directeurPlongee");?>> <br />
+        <label for="securiteSurface">Sécurité de surface : </label><input type="text" name="securiteSurface" placeholder="sécurité de surface?" <?php verifierRempli("securiteSurface");?>> <br />
 		<br />	   
 	  </fieldset>
         <br />
--- src/view/traitementPalanquee.php
+++ src/view/traitementPalanquee.php
@@ -2,56 +2,87 @@
 $erreur = true;
 if ( !empty($_POST ))	
 {
-	echo"<pre>";
-	print_r($_POST);
-	echo "</PRE>";
-	echo "décodage sur le serveur <br />";
-	  
-	$erreur = false;
-	if (!empty($_POST['date']) 
-    $date = $_POST['date'];  
+    $erreur = false;
+    
+	if (isset($_POST['datePalanquee'])) 
+        $datePalanquee = $_POST['datePalanquee'];  
 	else 
 	{
 		echo "la date n'est pas renseignée <br />";
 		$erreur = true;
-	}
-	if (isset($_POST['seance']) ) 
-		$pays = $_POST['seance'];  
+    }
+    
+	if (isset($_POST['seance'])) 
+		$seance = $_POST['seance'];  
 	else 
 	{
 		echo "la séance n'a pas été sélectionné <br />";
 		$erreur = true;
     }
+
     if (!empty($_POST['lieu']) && $_POST['lieu'] != "") 
-    $lieu = $_POST['lieu'] ; 
+        $lieu = $_POST['lieu'] ; 
     else 
     {
-        echo "le lieu est vide <br />";
+        echo "le lieu n'est pas renseigné <br />";
         $erreur = true;
     }
-    if (isset($_POST['embarcation']) ) 
+
+    if (isset($_POST['embarcation'])) 
 		$embarcation = $_POST['embarcation'];  
 	else 
 	{
 		echo "l'embarcation' n'a pas été sélectionné <br />";
 		$erreur = true;
-	}
-	if (!empty($_POST['discret']) ) 	
-		$val = $_POST['discret'];
-			
+    }
+    
+    if (!empty($_POST['effectifPlongeurs']))
+        $effectifPlongeurs = $_POST['effectifPlongeurs'] ; 
+    else 
+    {
+        echo "le nombre de plongeurs n'est pas renseigné <br />";
+        $erreur = true;
+    }
+
+    if (!empty($_POST['effectifBateau'])) 
+        $effectifBateau = $_POST['effectifBateau'] ; 
+    else 
+    {
+        echo "le nombre de personnes sur le bateau n'est pas renseigné <br />";
+        $erreur = true;
+    }
+
+    if (!empty($_POST['directeurPlongee']) && $_POST['directeurPlongee'] != "") 
+        $directeurPlongee = $_POST['directeurPlongee'] ; 
+    else 
+    {
+        echo "le directeur de la plongée n'est pas renseigné <br />";
+        $erreur = true;
+    }
+
+    if (!empty($_POST['securiteSurface']) && $_POST['securiteSurface'] != "") 
+        $securiteSurface = $_POST['securiteSurface'] ; 
+    else 
+    {
+        echo "la sécurité de surface n'est pas renseigné <br />";
+        $erreur = true;
+    }
+    
+    
 	if ($erreur == false)
 	{
-        echo "date : $date <br />";
+        echo "date : $datePalanquee <br />";
         echo "séance : $seance <br />";
         echo "lieu : $lieu <br />";
         echo "embarcation : $embarcation <br />";
-		echo "CIVILITE : $civ <br />";
-		echo "PAYS : $pays <br />";
+		echo "effectif de plongeurs : $effectifPlongeurs <br />";
+        echo "effectif du bateau : $effectifBateau <br />";
+        echo "directeur de plongée : $directeurPlongee <br />";
+        echo "sécurité de surface : $securiteSurface <br />";
 	}
 }
 if ($erreur == true)
 {
-	include ("util.php");
 	include ("palanquee.php")	 ;
 }
 ?>
