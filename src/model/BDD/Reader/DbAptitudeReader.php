7a6f526d8f46bf2fd61ddcdc1c98442c68b3d4af
yodaluc <luc.audabram1@gmail.com>
2019-09-25 17:11:10 +0200

    Create DbAptitudeReader.php

---
+++ src/model/BDD/Reader/DbAptitudeReader.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once('../DbConnector.php');
+
+class DbAptitudeReader
+{
+    private $dbConnector;
+
+    public function __construct()
+    {
+        $this->dbConnector = new DbConnector();
+    }
+
+    public function getAllAptitude()
+    {
+        try {
+            $pdo = $this->dbConnector->getConnection();
+        } catch (Exception $e) {
+            return false;
+        }
+
+        $statement = $this->dbConnector->prepStatement($pdo,"SELECT * FROM `plo_aptitude`");
+
+        return $this->dbConnector->execStatement($statement);
+    }
+}
