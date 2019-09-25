8f92d3f2a16c203c2379dc95d7ca4066cf23cd13
yodaluc <luc.audabram1@gmail.com>
2019-09-25 16:45:27 +0200

    Ajout DbDirecteurWriter et DbSecuSurfaceWriter

---
+++ src/model/BDD/DbSecuSurfaceWriter.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once('DbConnector.php');
+
+class DbSecuSurfaceWriter
+{
+    private $dbConnector;
+
+    public function __construct()
+    {
+        $this->dbConnector = new DbConnector();
+    }
+
+    public function addPersonne($PER_NUM)
+    {
+        try
+        {
+            $pdo = $this->dbConnector->getConnection();
+        } catch (Exception $e)
+        {
+            echo $e;
+            return false;
+        }
+
+        $statement = $this->dbConnector->prepStatement($pdo,"INSERT INTO `plo_securite_de_surface`(`PER_NUM`) VALUES (:PER_NUM);");
+
+        $statement->bindParam(':PER_NUM', $PER_NUM);
+
+        $res = $this->dbConnector->execStatement($statement);
+        return $res;
+    }
+}
