d8f54771ad5392d28b48b87dad49a72ddecf1c8a
--global <matthisrivat@gmail.com>
2019-09-25 17:32:56 +0200

    ajout acceuil et modifi personne

--- src/view/Accueil.php
+++ src/view/Accueil.php
@@ -8,9 +8,9 @@
 <body>
 <?php require_once("Header.php"); ?>
 
-<input type="button" value="Ajouter un plongeur ou modifier ses données personnelles"></br>
-<input type="button" value="Ajouter une palanquée"></br>
-<input type="button" value="Historique des palanquées"></br>
+<input type="button" value="Ajouter un plongeur ou modifier ses données personnelles" onclick="window.location.href='AjouterOuModifierDonneePlonjeur.php'"></br>
+<input type="button" value="Ajouter une palanquée" onclick="window.location.href='palanquee.php'"></br>
+<input type="button" value="Historique des palanquées" onclick="window.location.href='HistoriqueDesPalanquees.php'"></br>
 
 <?php require_once("Footer.php"); ?>
 </body>
--- src/view/AjouterOuModifierDonneePlonjeur.php
+++ src/view/AjouterOuModifierDonneePlonjeur.php
@@ -1,15 +1,35 @@
-<!DOCTYPE html>
-<html lang="fr">
-<meta charset="UTF-8"/>
-<head>
-    <title>Gestion de plongée - Accueil</title>
-    <!-- head definitions go here -->
-</head>
-<body>
 <?php require_once("Header.php"); ?>
+<form name="palanquee" action="" method="post" enctype="multipart/form-data">
+    <fieldset>
+        <legend><b>Ajouter un plonjeur</b></legend>
+        <br/>
+        <label for="nom">Nom: </label><input type="text" name="nom" size="30" maxlength="50"><br/>
+        <label for="prenom">Prenom: </label><input type="text" name="Prénom" size="30" maxlength="50"><br/>
+        <label for="aptitude">Aptitude: </label>
+        <select id="aptitude">
+            <option value="">--Please choose an option--</option>
+            <?php
+            
+            $aptitude = selectAptiude();
 
+            foreach ($aptitude as $item) {
+                $labelle = $item[0];
+                $code = $item[1];
+                $option = "<option value=\"" . $code + "\">" . $labelle . "</option>";
+
+
+                //">Dog;
+            }
+            ?>
+        </select>
+
+
+    </fieldset>
+    <br/>
+    <input type="submit" name="EN" value="Ajouter" onclick="return testerValid()"> &nbsp;&nbsp;&nbsp;
+    <br/>
+    <br/>
+</form>
 
 
 <?php require_once("Footer.php"); ?>
-</body>
-</html>
