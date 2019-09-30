e387665c41ce116b5de08032f88ae2142c81cb8b
yodaluc <luc.audabram1@gmail.com>
2019-09-30 13:12:52 +0200

    Update template.php

--- src/view/template.php
+++ src/view/template.php
@@ -1,1 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=utf-8" />
+        <title><?php echo $this->_title ?></title>
+    </head>
+    <body>
+        <div id="header">
+            <?php require_once 'templateInfo/header.php'; ?>
+        </div>
+        <?php echo $content ?>
+        <?php require_once 'templateInfo/Footer.php'; ?>
+    </body>
+</html>
