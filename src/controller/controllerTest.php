f7ceb524f815e6b2d170a34361c7bab925996ebc
yodaluc <luc.audabram1@gmail.com>
2019-09-30 13:55:04 +0200

    update

---
+++ src/controller/controllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class controllerTest
+{
+    private $_view;
+
+    public function __construct($url)
+    {
+        if(isset($url) && count($url) > 1)
+        {
+            throw new Exception('Page introuvable');
+        }
+        else
+        {
+            $this->mentions();
+        }
+    }
+
+    public function mentions()
+    {
+        $this->_view = new View('Test');
+        $this->_view->generate(array());
+    }
+}
