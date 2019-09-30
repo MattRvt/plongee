f9afbe0b322e81f928265eed471cd4f02e3fd7b9
yodaluc <luc.audabram1@gmail.com>
2019-09-30 13:02:57 +0200

    ajout du routeur

--- src/view/test.php
+++ src/view/test.php
@@ -1,5 +1,12 @@
 <?php
 require_once ("Header.php");
 
+include("../model/BDD/Reader/DbAptitudeReader.php");
+
+$Reader = new DbAptitudeReader();
+$suc = $Reader->getAllAptitude();
+echo "<pre>";
+print_r($suc);
+echo "</pre>";
 require_once ("Footer.php");
 ?>
