098414346d9d532d5530bd9cdaf13917f3bbc22f
--global <matthisrivat@gmail.com>
2019-09-30 13:08:28 +0200

    liste dans page plongeur

--- src/view/AjouterOuModifierDonneePlonjeur.php
+++ src/view/AjouterOuModifierDonneePlonjeur.php
@@ -1,35 +1,37 @@
 <?php require_once("Header.php"); ?>
-<form name="palanquee" action="" method="post" enctype="multipart/form-data">
-    <fieldset>
-        <legend><b>Ajouter un plonjeur</b></legend>
+    <form name="palanquee" action="" method="post" enctype="multipart/form-data">
+        <fieldset>
+            <legend><b>Ajouter un plonjeur</b></legend>
+            <br/>
+            <label for="nom">Nom: </label><input type="text" name="nom" size="30" maxlength="50"><br/>
+            <label for="prenom">Prenom: </label><input type="text" name="Prénom" size="30" maxlength="50"><br/>
+            <label for="aptitude">Aptitude: </label>
+            <select id="aptitude">
+                <option value="">--Please choose an option--</option>
+                <?php
+                include("../model/BDD/Reader/DbAptitudeReader.php");
+                $Reader = new DbAptitudeReader();
+                $aptitude = $Reader->getAllAptitude();
+
+                foreach ($aptitude as $item) {
+                    $labelle = $item["APT_LIBELLE"];
+
+                    $code = $item["APT_CODE"];
+
+                    $option = "<option value='$code'>$labelle</option>";
+                    echo $option;
+                }
+                echo "</selcet>";
+                ?>
+            </select>
+
+
+        </fieldset>
         <br/>
-        <label for="nom">Nom: </label><input type="text" name="nom" size="30" maxlength="50"><br/>
-        <label for="prenom">Prenom: </label><input type="text" name="Prénom" size="30" maxlength="50"><br/>
-        <label for="aptitude">Aptitude: </label>
-        <select id="aptitude">
-            <option value="">--Please choose an option--</option>
-            <?php
-            
-            $aptitude = selectAptiude();
-
-            foreach ($aptitude as $item) {
-                $labelle = $item[0];
-                $code = $item[1];
-                $option = "<option value=\"" . $code + "\">" . $labelle . "</option>";
-
-
-                //">Dog;
-            }
-            ?>
-        </select>
-
-
-    </fieldset>
-    <br/>
-    <input type="submit" name="EN" value="Ajouter" onclick="return testerValid()"> &nbsp;&nbsp;&nbsp;
-    <br/>
-    <br/>
-</form>
+        <input type="submit" name="EN" value="Ajouter" onclick="return testerValid()"> &nbsp;&nbsp;&nbsp;
+        <br/>
+        <br/>
+    </form>
 
 
 <?php require_once("Footer.php"); ?>
--- src/view/test.php
+++ src/view/test.php
@@ -1,5 +1,25 @@
 <?php
-require_once ("Header.php");
+require_once("Header.php");
 
-require_once ("Footer.php");
+include("../model/BDD/Reader/DbAptitudeReader.php");
+$Reader = new DbAptitudeReader();
+$aptitude = $Reader->getAllAptitude();
+echo "<pre>";
+//print_r($aptitude);
+echo "</pre>";
+?>
+<select id=\"aptitude\">
+                <option value=\"\">--Please choose an option--</option>
+    <?php
+foreach ($aptitude as $item) {
+    $labelle = $item["APT_LIBELLE"];
+
+    $code = $item["APT_CODE"];
+
+    $option = "<option value='$code'>$labelle</option>";
+    echo $option;
+}
+echo "</selcet>";
+
+require_once("Footer.php");
 ?>
