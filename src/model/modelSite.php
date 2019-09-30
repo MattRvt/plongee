ec5238401dc8b54d8898a206228e0b0bbbfc872c
yodaluc <luc.audabram1@gmail.com>
2019-09-30 14:08:32 +0200

    update model

--- src/model/modelSite.php
+++ src/model/modelSite.php
@@ -5,16 +5,9 @@ class modelSite extends model
 {
     public function addPersonne($SIT_NUM,$SIT_NOM,$SIT_LOCALISATION)
     {
-        try
-        {
-            $pdo = $this->dbConnector->getConnection();
-        } catch (Exception $e)
-        {
-            echo $e;
-            return false;
-        }
+        $this->getBdd();
 
-        $statement = $this->dbConnector->prepStatement($pdo,"INSERT INTO `site`(`SIT_NUM`, `SIT_NOM`, `SIT_LOCALISATION`) VALUES (:SIT_NUM,:SIT_NOM,:SIT_LOCALISATION)");
+        $statement = $this->dbConnector->prepare("INSERT INTO `site`(`SIT_NUM`, `SIT_NOM`, `SIT_LOCALISATION`) VALUES (:SIT_NUM,:SIT_NOM,:SIT_LOCALISATION)");
 
         $statement->bindParam(':SIT_NUM', $SIT_NUM);
         $statement->bindParam(':SIT_NOM', $SIT_NOM);
