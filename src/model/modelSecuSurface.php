bf39938d3e08b36ab6340fca2ee4299261f9e7a6
yodaluc <luc.audabram1@gmail.com>
2019-09-30 14:10:38 +0200

    update model

--- src/model/modelSecuSurface.php
+++ src/model/modelSecuSurface.php
@@ -4,8 +4,7 @@ class modelSecuSurface extends model
 {
     public function addPersonne($PER_NUM)
     {
-
-        $statement = $this->getBdd()->prepStatement($pdo,"INSERT INTO `plo_securite_de_surface`(`PER_NUM`) VALUES (:PER_NUM)");
+        $statement = $this->getBdd()->prepStatement("INSERT INTO `plo_securite_de_surface`(`PER_NUM`) VALUES (:PER_NUM)");
 
         $statement->bindParam(':PER_NUM', $PER_NUM);
 
--- src/model/modelSite.php
+++ src/model/modelSite.php
@@ -5,15 +5,13 @@ class modelSite extends model
 {
     public function addPersonne($SIT_NUM,$SIT_NOM,$SIT_LOCALISATION)
     {
-        $this->getBdd();
-
-        $statement = $this->dbConnector->prepare("INSERT INTO `site`(`SIT_NUM`, `SIT_NOM`, `SIT_LOCALISATION`) VALUES (:SIT_NUM,:SIT_NOM,:SIT_LOCALISATION)");
+        $statement = $this->getBdd()->prepare("INSERT INTO `site`(`SIT_NUM`, `SIT_NOM`, `SIT_LOCALISATION`) VALUES (:SIT_NUM,:SIT_NOM,:SIT_LOCALISATION)");
 
         $statement->bindParam(':SIT_NUM', $SIT_NUM);
         $statement->bindParam(':SIT_NOM', $SIT_NOM);
         $statement->bindParam(':SIT_LOCALISATION', $SIT_LOCALISATION);
 
-        $res = $this->dbConnector->execStatement($statement);
+        $res = $this->getBdd()->execStatement($statement);
         return $res;
     }
 }
