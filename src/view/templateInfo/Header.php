a4ea277498ae99f84bd4ef8721f8ecb88f2b3a6b
yodaluc <luc.audabram1@gmail.com>
2019-09-30 13:12:34 +0200

    changement de position des header et footer

--- src/view/templateInfo/Header.php
+++ src/view/templateInfo/Header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>SUBALLIGATOR</title>
-        <script src="fonctions.js"></script>
+        <script src="../fonctions.js"></script>
     </head>
     <body>
 <?php require_once("../model/Settings.php");
