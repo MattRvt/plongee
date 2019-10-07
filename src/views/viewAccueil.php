01e1ca6482f8e3546be0916ba6484980c7ed0107
--global <matthisrivat@gmail.com>
2019-10-07 16:42:07 +0200

    merge

--- src/views/viewAccueil.php
+++ src/views/viewAccueil.php
@@ -4,3 +4,5 @@
 <input type="button" value="Ajouter une palanquée" onclick="window.location.href='Palanquee'"></br>
 <input type="button" value="voir ou éditer une palanquées" onclick="window.location.href='EditionPalanquee'"></br>
 <input type="button" value="Historique des palanquées" onclick="window.location.href='HistoriqueDesPalanquees'"></br>
+<input type="button" value="test" onclick="window.location.href='test'"></br>
+
--- src/views/viewTest.php
+++ src/views/viewTest.php
@@ -1,3 +1,9 @@
 <?php
 
+
+$listePlongee = $this->controller->getPongee();
+echo "<pre>";
+
+print_r ($listePlongee);
+echo "</pre>"
 ?>
