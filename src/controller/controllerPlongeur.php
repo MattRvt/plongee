0413e1c35f82ce4206e9fd6631a80a6c86277934
yodaluc <luc.audabram1@gmail.com>
2019-10-07 15:21:06 +0200

    modification de strcture du plongeur

--- src/controller/controllerPlongeur.php
+++ src/controller/controllerPlongeur.php
@@ -1,33 +1,7 @@
 <?php
 
+
 class controllerPlongeur
 {
-    private $_view;
-
-    public function __construct($url)
-    {
-        if (isset($url) && count($url) > 1) {
-            throw new Exception('Page introuvable');
-        } else {
-            $this->mentions();
-        }
-    }
-
-    public function mentions()
-    {
-        $this->_view = new View('Plongeur');
-        $this->_view->generate(array(),$this);
-    }
-
-    public function selectAptitude()
-    {
-        require_once('model/modelAptitude.php');
-        $reader = new modelAptitude();
-        $aptitude = $reader->getAll();
-        return $aptitude;
-    }
-
 
 }
-
-?>
