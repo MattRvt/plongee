d2fabf77dbacd39cfd6dcd760f7246e11aa20cb1
--global <matthisrivat@gmail.com>
2019-10-08 09:24:33 +0200

    merge

--- src/views/viewAccueil.php
+++ src/views/viewAccueil.php
@@ -4,5 +4,6 @@
 <input type="button" value="Ajouter une palanquée" onclick="window.location.href='Palanquee'"></br>
 <input type="button" value="voir ou éditer une palanquées" onclick="window.location.href='EditionPalanquee'"></br>
 <input type="button" value="Historique des palanquées" onclick="window.location.href='HistoriqueDesPalanquees'"></br>
+<input type="button" value="plongee" onclick="window.location.href='Plongee'"></br>
 <input type="button" value="test" onclick="window.location.href='test'"></br>
 
