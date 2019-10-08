a39aa78139f905f248fd929acf3c6fa8338444d7
Melissande <smelissande@gmail.com>
2019-10-08 08:50:53 +0200

    Correction erreurs

--- src/views/viewAccueil.php
+++ src/views/viewAccueil.php
@@ -1,8 +1,8 @@
 <?php $this->_title = "Accueil" ?>
 
-<input type="button" value="ListePersonne" onclick="window.location.href='ListePersonne'"></br>
-<input type="button" value="Ajouter une palanquée" onclick="window.location.href='Palanquee'"></br>
-<input type="button" value="voir ou éditer une palanquées" onclick="window.location.href='EditionPalanquee'"></br>
-<input type="button" value="Historique des palanquées" onclick="window.location.href='HistoriqueDesPalanquees'"></br>
-<input type="button" value="test" onclick="window.location.href='test'"></br>
+<input type="button" value="ListePersonne" onclick="window.location.href='ListePersonne'"><br/>
+<input type="button" value="Ajouter une palanquée" onclick="window.location.href='Palanquee'"><br/>
+<input type="button" value="voir ou éditer une palanquées" onclick="window.location.href='EditionPalanquee'"><br/>
+<input type="button" value="Historique des palanquées" onclick="window.location.href='HistoriqueDesPalanquees'"><br/>
+<input type="button" value="test" onclick="window.location.href='test'"><br/>
 
