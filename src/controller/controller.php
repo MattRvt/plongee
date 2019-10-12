966fd35ae89d6d45855ead8ebeadd5e2c76d19ae
--global <matthisrivat@gmail.com>
2019-10-12 21:01:17 +0200

    creation de controller.php et ajout de la methode listeDerouante
    renvoie la liste des option avec les donnes d'une table.

---
+++ src/controller/controller.php
@@ -0,0 +1,24 @@
+<?php
+
+
+abstract class controller
+{
+    /**
+     * A utilisé dans une balise select
+     * renvoie la liste des option avec les donnes d'une table.
+     * @param $reader model servant à lire la base
+     * @param $label colonne qui sera utilisé pour afficher sur la page
+     * @param $code valeur du select (value)
+     */
+    public function listeDeroulante($reader, $label, $code)
+    {
+        echo "<option value=\"null\">--Please choose an option--</option>";
+        $req = $reader->getAll();
+        $values = $req;
+        foreach ($values as $ligne) {
+            $labelOption = $ligne[$label];
+            $codeOption = $ligne[$code];
+            echo "<option value=\"" . $codeOption . "\" > " . $labelOption . "</option>";
+        }
+    }
+}
