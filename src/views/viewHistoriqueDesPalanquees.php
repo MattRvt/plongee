413739a64351e4d7e4442d63076ac791d28b70e0
yodaluc <luc.audabram1@gmail.com>
2019-10-14 14:11:21 +0200

    delete

--- src/views/viewHistoriqueDesPalanquees.php
+++
@@ -1,9 +0,0 @@
-<?php $this->_title = "historique des palanquées" ?>
-historique des palanquees etant partie en mer:
-<?php
-$palanquee = $this->controller->selectPalanquee();
-echo "<pre>";
-
-print_r ( $palanquee);
-echo "</pre>"
-?>
