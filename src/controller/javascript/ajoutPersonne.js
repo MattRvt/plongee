b29fe072b65a4640f884d72871cfa691baf0aaea
yodaluc <luc.audabram1@gmail.com>
2019-10-14 15:50:49 +0200

    update NewPlongeur.js

--- src/controller/javascript/ajoutPersonne.js
+++ src/controller/javascript/ajoutPersonne.js
@@ -8,16 +8,32 @@ var personne = {
 function addPersonne()
 {
     personne.nom = document.getElementById('nom').value;
-    personne.nom = document.getElementById('prenom').value;
-    personne.nom = document.getElementById('fonction').value;
-    personne.nom = document.getElementById('aptitude').value;
+    personne.prenom = document.getElementById('prenom').value;
+    personne.fonction = document.getElementById('fonction').value;
+    personne.aptitude = document.getElementById('aptitude').value;
+
+    $("#erreur").html("");
 
     var xhr = initXHR();
 
-    xhr.open('POST', 'index.php?url=AddPlongeurInBase', false);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send('');
-
-    //reset des messages
-    $("#erreur").html("");
+    if(personne.fonction != "" && personne.aptitude == "")
+    {
+        $("#erreur").html("Veuillez selectionner une aptitude");
+    }
+    else if(personne.fonction == "" && personne.aptitude != "")
+    {
+        $("#erreur").html("Veuillez selectionner une fonction");
+    }
+    else if(personne.fonction == "" && personne.aptitude == "")
+    {
+        xhr.open('POST', 'index.php?url=AddPersonneInBase', false);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send('');
+    }
+    else
+    {
+        xhr.open('POST', 'index.php?url=AddPlongeurInBase', false);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send('');
+    }
 }
