996b83c816196c5c808e403a144945fa642368b9
NathanC <21716210@etu.unicaen.fr>
2019-10-14 13:31:47 +0200

    modification ou suppression de personnes avec redirection

--- src/views/viewModifierPlongeur.php
+++ src/views/viewModifierPlongeur.php
@@ -2,19 +2,24 @@
 $num = $_GET["param"];
 require_once ('model/modelPersonne.php');
 ?>
-Numero d'identification : <?php $Personne = $this->controller->selectPersonne($num); //Apres recup $_POST['PER_NUM'] et echo
-            $numPersonne = $Personne['PER_NUM'];
-            echo $numPersonne."<br/>" ;?>
-Nom : <?php $nomPersonne = $Personne['PER_NOM'];
-            echo "<input type=\"text\" value =".$nomPersonne." id=\"nom\"/> <br/>" ;?>
-Prénom : <?php $prenomPersonne = $Personne['PER_PRENOM'];
-            echo "<input type=\"text\" value =".$prenomPersonne." id=\"prenom\"/> <br/>" ;?> <br/>
-<?php
-$instancePersonne = new modelPersonne();
-$Plonger = $instancePersonne->isPlongeur($numPersonne);
-if (!empty($Plonger)){
-    echo 'Aptitude :';
-    $Plongeur = $this->controller->selectPlongeur($numPersonne);
-    $aptitudePlongeur = $Plongeur['APT_CODE'];
-    echo $aptitudePlongeur .'<br/>';
-}?>
+<form action="index.php" method="get">
+    Numero d'identification : <?php $Personne = $this->controller->selectPersonne($num); //Apres recup $_POST['PER_NUM'] et echo
+                $numPersonne = $Personne['PER_NUM'];
+                echo $numPersonne."<br/>" ;?>
+    Nom : <?php $nomPersonne = $Personne['PER_NOM'];
+                echo "<input type=\"text\" value =".$nomPersonne."  id=\"nom\" name=\"nom\"/> <br/>" ;?>
+    Prénom : <?php $prenomPersonne = $Personne['PER_PRENOM'];
+                echo "<input type=\"text\" value =".$prenomPersonne."  id=\"prenom\" name=\"prenom\"/> <br/>" ;?> <br/>
+    <?php
+    $instancePersonne = new modelPersonne();
+    $Plonger = $instancePersonne->isPlongeur($numPersonne);
+    if (!empty($Plonger)){
+        echo 'Aptitude :';
+        $Plongeur = $this->controller->selectPlongeur($numPersonne);
+        $aptitudePlongeur = $Plongeur['APT_CODE'];
+        echo $aptitudePlongeur .'<br/>';
+    }?>
+    <input type="submit" value="Modifier" name="modifier"/>
+    <input type="submit" value="Supprimer" name="supprimer"/>
+</form>
+
