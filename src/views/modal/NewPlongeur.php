b1ca290cc3e7c8f9f7c4730d7024a97b374cc782
yodaluc <luc.audabram1@gmail.com>
2019-10-14 14:57:36 +0200

    updae new plongeur

--- src/views/modal/NewPlongeur.php
+++ src/views/modal/NewPlongeur.php
@@ -4,39 +4,30 @@ $controller = new controllerNewPlongeur();
 ?>
 <div class="modal" id="newPlongeurModal">
     <div class="modal-content">
-        <form name="palanquee" action="controller/traitementPlongeur" method="post" enctype="multipart/form-data">
-            <fieldset>
+        <form name="plongeur" action="../controller/controllerNewPlongeur" method="post" enctype="multipart/form-data">
                 <legend><b>Ajouter une personne</b></legend>
                 <br/>
-                <label for="nom">Nom: </label><input type="text" name="nom" size="30" maxlength="50" placeholder="Nom"
-                                                     value="<?php $controller->verifierRempli("nom"); ?>"
-                                                     required><br/>
-                <label for="prenom">Prenom: </label><input type="text" name="prenom" size="30" maxlength="50"
-                                                           placeholder="Prénom"
-                                                           value="<?php $controller->verifierRempli("prenom"); ?>"
-                                                           required><br/>
+
+                <label for="nom">Nom: </label><input type="text" name="nom" size="30" maxlength="50" placeholder="Nom" value="<?php $controller->verifierRempli("nom");?>"><br/>
+                <label for="prenom">Prenom: </label><input type="text" name="prenom" size="30" maxlength="50" placeholder="Prénom" value="<?php $controller->verifierRempli("prenom");?>" ><br/>
+
                 <label for="fonction">Fonction<label>
                         <select id="fonction">
-                            <option value="rien" hidden>--Please choose an option--</option>
-                            <option value="plongeur" <?php $controller->VerifSelectFonction("fonction", "plongeur"); ?>>
-                                Plongeur
-                            </option>
-                            <option value="directeur" <?php $controller->VerifSelectFonction("fonction", "directeur"); ?>>
-                                Directeur
-                            </option>
-                            <option value="securiteSurface" <?php $controller->VerifSelectFonction("fonction", "securiteSurface"); ?>>
-                                Sécurité de surface
-                            </option>
+                            <option value="rien">--Please choose an option--</option>
+                            <option value="plongeur" <?php $controller->VerifSelectFonction("fonction","plongeur"); ?>>Plongeur</option>
+                            <option value="directeur" <?php $controller->VerifSelectFonction("fonction","directeur"); ?>>Directeur</option>
+                            <option value="securiteSurface" <?php $controller->VerifSelectFonction("fonction","securiteSurface"); ?>>Sécurité de surface</option>
                         </select><br/>
+
                         <label for="aptitude">Aptitude: </label>
                         <select id="aptitude">
-                            <?php echo $controller->selectAptitude(); ?>
+                            <option value="">--Please choose an option--</option>
+                            <?php
+                                $aptitude = $this->controller->selectAptitude();
+                            ?>
                         </select>
-            </fieldset>
-            <br/>
+            <br />
             <input type="submit" name="EN" value="Envoyer" onclick="return testerValid()">
-            <br/>
-            <br/>
         </form>
     </div>
 </div>
