8ed84102117d903bef9ffb68d662a4d919848ed8
yodaluc <luc.audabram1@gmail.com>
2019-10-14 13:41:28 +0200

    update nexplongeur

--- src/controller/controllerNewPlongeur.php
+++ src/controller/controllerNewPlongeur.php
@@ -25,6 +25,32 @@ class controllerNewPlongeur extends controller
         $reader = new modelAptitude();
         echo $this->listeDeroulante($reader, "APT_LIBELLE","APT_CODE");
     }
+
+    function verifierRempli($n)
+    {
+        if (isset($_POST[$n])) {
+            $var = $_POST[$n];
+            if ($var <> "")
+                echo $var;
+        } else
+            echo "";
+    }
+
+    function VerifSelectAptitude($n)
+    {
+        if (isset($_POST["seance"])) {
+            if ($_POST["seance"] == $n)
+                echo "selected";
+        }
+    }
+
+    function VerifSelectFonction($n)
+    {
+        if (isset($_POST["fonction"])) {
+            if ($_POST["fonction"] == $n)
+                echo "selected";
+        }
+    }
 }
 
 ?>
