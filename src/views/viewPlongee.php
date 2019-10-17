7b2329e1a3b211279aae6b3b333dea3c406731ca
--global <matthisrivat@gmail.com>
2019-10-17 08:36:11 +0200

    suppression champs inutile dans plongee

--- src/views/viewPlongee.php
+++ src/views/viewPlongee.php
@@ -38,35 +38,6 @@
                value="<?php $this->controller->verifierRempli("etat"); ?>"
         ><br/>
     </fieldset>
-    <fieldset>
-        <legend><b>données prévu</b></legend>
-        <label for="hDepart">heure de départ: </label>
-        <input type="time" name="hDepart" value="<?php $this->controller->verifierRempli("hDepart"); ?>"
-        ><br/>
-        <label for="tpsPervu">temps prévu: </label>
-        <input type="time" name="tpsPervu" value="<?php $this->controller->verifierRempli("tpsPervu"); ?>"
-        ><br/>
-        <label for="profondeurPrevu">profondeur prévu </label>
-        <input type="number" name="profondeurPrevu"
-               value="<?php $this->controller->verifierRempli("profondeurPrevu"); ?>"
-        ><br/>
-    </fieldset>
-    <fieldset>
-        <legend><b>données realisé</b></legend>
-        <label for="hArrivee">heure d'arrivé: </label>
-        <input type="time" name="hArrivee"
-               value="<?php $this->controller->verifierRempli("hArrivee"); ?>"
-        ><br/>
-        <label for="tpsRealise">temps réalisé: </label>
-        <input type="time" name="tpsRealise"
-               value="<?php $this->controller->verifierRempli("tpsRealise"); ?>"
-        ><br/>
-        <label for="profondeurRealise">profondeur réalisé: </label>
-        <input type="number" name="profondeurRealise"
-               value="<?php $this->controller->verifierRempli("profondeurRealise"); ?>"
-
-        ><br/>
-    </fieldset>
     <-- liste des palanquées -->
     <fieldset>
         <legend><b>enregistrer:</b></legend>
