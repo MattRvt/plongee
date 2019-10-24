7a2205e9a6f6fd0f39a2ab732d5841dc0c16ccad
NathanC <21716210@etu.unicaen.fr>
2019-10-24 09:57:35 +0200

    affichage des personne en tab js

--- src/controller/javascript/listePersonne.js
+++ src/controller/javascript/listePersonne.js
@@ -6,13 +6,38 @@ function initListPers()
 
 function updatePlongeur()
 {
-    var xhr = initXHR();
+    $(document).ready(function(){
+        $.ajax({
+            url: 'ListePlongeur',
+            type: 'get',
+            dataType: 'JSON',
+            success: function(response){
+                var len = response.length;
+                for(var i=0; i<len; i++){
+                    var num = response[i].PER_NUM;
+                    var nom = response[i].PER_NOM;
+                    var prenom = response[i].PER_PRENOM;
 
-    xhr.open('POST', 'index.php?url=ListePlongeur', false);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send();
+                    var actif = response[i].PER_ACTIVE;
+                    var certif = response[i].PER_DATE_CERTIF_MED;
+                    var aptcode = response[i].APT_CODE;
 
-    $("#listPlongeur").html(xhr.responseText);
+                    var tr_str = "<tr>" +
+                        "<td align='center'>" + num + "</td>" +
+                        "<td align='center'>" + nom + "</td>" +
+                        "<td align='center'>" + prenom + "</td>" +
+                        "<td align='center'>" + actif + "</td>" +
+                        "<td align='center'>" + certif + "</td>" +
+                        "<td align='center'>" + aptcode + "</td>" +
+                        "<td align='center'> <input type='button' value='Modifier Plongeur' onclick=\"window.location.href='ModifierPlongeur&param=" + num + "'\"> </td>" +
+                        "</tr>";
+
+                    $("#userTable tbody").append(tr_str);
+                }
+
+            }
+        });
+    });
 }
 
 function updateNonPlongeur()
