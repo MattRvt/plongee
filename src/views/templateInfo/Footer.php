160d542d1de1876c7396ae8fa7e2cf9ca5148674
--global <matthisrivat@gmail.com>
2019-10-29 17:16:06 +0100

    Update Footer.php

--- src/views/templateInfo/Footer.php
+++ src/views/templateInfo/Footer.php
@@ -7,7 +7,7 @@
                 <p class="grey-text text-lighten-4">Projet réalisé dans le cadre du cours de PHP de 2ème année d'IUT informatique.<br><br>© 2019 Copyright SubAligator Bretagne, Inc. All rights reserved.</p>
             </div>
             <div class="col s5">
-                <h5 class="white-text">Étudiants</h5>
+                <h5 class="white-text">Étudiants:</h5>
                 <div class="col s12 m6 l3 truncate"><p class="grey-text text-lighten-3">AUDABRAM<br> Luc</p></div>
                 <div class="col s12 m6 l3 truncate"><p class="grey-text text-lighten-3">CHAVAS<br> Nathan</p></div>
                 <div class="col s12 m6 l3 truncate"><p class="grey-text text-lighten-3">RIVAT<br> Matthis</p></div>
