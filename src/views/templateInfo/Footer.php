493469a0ada871092c3fe6ad46a94e8daf9d9f4b
Akodix <21716210@etu.unicaen.fr>
2019-10-29 16:34:18 +0100

    footer gg + template

--- src/views/templateInfo/Footer.php
+++ src/views/templateInfo/Footer.php
@@ -1,5 +1,18 @@
+
 <footer class="page-footer blue">
-    <div class="center-align">
-        <a class="footerText">SubAligator</a>
-    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col s7">
+                <h5 class="white-text">Sub'Aligator</h5>
+                <p class="grey-text text-lighten-4">Projet réalisé dans le cadre du cours de PHP de 2ème année d'IUT informatique.<br><br>© 2014 Copyright Text</p>
+            </div>
+            <div class="col s5">
+                <h5 class="white-text">Étudiants</h5>
+                <div class="col s12 m6 l3 truncate"><p class="grey-text text-lighten-3">AUDABRAM<br> Luc</p></div>
+                <div class="col s12 m6 l3 truncate"><p class="grey-text text-lighten-3">CHAVAS<br> Nathan</p></div>
+                <div class="col s12 m6 l3 truncate"><p class="grey-text text-lighten-3">RIVAT<br> Matthis</p></div>
+                <div class="col s12 m6 l3 truncate"><p class="grey-text text-lighten-3">SIMONIN<br> Melissande</p></div>
+            </div>
+            </div>
+        </div>
 </footer>
