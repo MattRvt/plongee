2fb8325862cb03ba1ea35fc3daf0e20fea979df3
--global <matthisrivat@gmail.com>
2019-11-04 11:57:15 +0100

    ajout de bouton imprimer non fonctionnel

---
+++ src/controller/controllerFicheSecurite.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class controllerFicheSecurite
+{
+    public function __construct($url)
+    {
+        if (isset($url) && count($url) > 1) {
+            throw new Exception('Page introuvable');
+        } else {
+            $this->FicheSecurite();
+        }
+    }
+
+    private function FicheSecurite()
+    {
+        $this->_view = new View('FicheSecurite');
+        $this->_view->generate(array(), $this);
+
+    }
+
+}
--- src/controller/javascript/listePalanquee.js
+++ src/controller/javascript/listePalanquee.js
@@ -254,3 +254,11 @@ function estPasserOuPas(datePal, matMidSoi)
 
     return dateP>date;
 }
+
+function printExternal(url) {
+    var printWindow = window.open( url, 'Print', 'left=200, top=200, width=950, height=500, toolbar=0, resizable=0');
+    printWindow.addEventListener('load', function(){
+       // printWindow.print();
+        //printWindow.close();
+    }, true);
+}
