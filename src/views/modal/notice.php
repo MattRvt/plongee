8dc3775a8519e375c898281532dba6ae67ab64b9
--global <matthisrivat@gmail.com>
2019-11-09 11:05:49 +0100

    Revert "ajout FAQ"
    This reverts commit cbb77092f61002f24ad7222aba41a5c9de07b719.

--- src/views/modal/notice.php
+++ src/views/modal/notice.php
@@ -1,20 +1,9 @@
-<div class="genModal modal" id="notice">
+<div class="aesModal modal" id="notice">
     <div class="modal-content">
         <legend><b id="notice"></b></legend>
 
         <div>
-            <p><b>Comment accéder aux plongées affichées sur la page d'acceuil ? </b></p>
-            Les plongées affichées sur la page d'accueil sont accessibles depuis l'onglet plongée.
-            <p></p
-            <p><b>Pourquoi toutes les plongées, les sites, les embarcations, les aptitudes ou les personnes ne sont pas
-                    supprimables ?</b></p>
-            Une plongée ne peut être supprimé que si la plongée n'est pas complète.<br>
-            Les sites, les embarcations, les aptitudes et les personnes ne peuvent être supprimés que s'ils ne font pas
-            partie d'une plongée ou que personne ne possède l'aptitude à supprimer.
-            <p></p>
-            <p><b>Pourquoi je ne peux pas ajouter de palanquer à une plongée ?</b></p>
-            Pour ajouter des palanquées à une plongée, il faut que celle-ci ne soit pas passée. Il est impossible d'ajouter des palanquées à une plongée qui a déjà eu lieu.
-            <p></p>
+            le texte explicatif
         </div>
     </div>
 </div>
