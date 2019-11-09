52e9eac129cfaf1b78dd858dff0778c509d20990
NathanC <21716210@etu.unicaen.fr>
2019-11-09 11:47:27 +0100

    nique le php à 8h du mat

--- src/views/modal/notice.php
+++ src/views/modal/notice.php
@@ -4,11 +4,12 @@
 
         <div>
             <p><b>Comment accéder aux plongées affichées sur la page d'acceuil ? </b></p>
-            Les plongées affichées sur la page d'accueil sont accessibles depuis l'onglet plongée.
+            Les plongées affichées sur l'accueil sont un historique des 3 dernières et des 3 prochaines plongées.<br>
+            Pour plus de détails, l'ensemble des plongées est accessible depuis l'onglet "Plongée".
             <p></p
             <p><b>Pourquoi toutes les plongées, les sites, les embarcations, les aptitudes ou les personnes ne sont pas
                     supprimables ?</b></p>
-            Une plongée ne peut être supprimé que si la plongée n'est pas complète.<br>
+            Une plongée ne peut être supprimée que si la plongée n'est pas complète.<br>
             Les sites, les embarcations, les aptitudes et les personnes ne peuvent être supprimés que s'ils ne font pas
             partie d'une plongée ou que personne ne possède l'aptitude à supprimer.
             <p></p>
